0127d07744bc571cbe7e8bcd92bfd854f1e65da0
BTejesh27 <bvst27@gmail.com>
2025-04-08 19:36:56 +0530

    api structure

---
+++ src/pages/home/api/home.ts
@@ -0,0 +1,26 @@
+export const departmentOverview = {
+    title: "Department Overview",
+    description:
+      "The Department of Computer Science & Design and Computer Science & Information Technology is committed to excellence in teaching, research, and innovation.",
+  };
+  
+  export const announcements = [
+    "New Research Lab Opening Ceremony - March 15, 2025",
+    "International Conference on Emerging Technologies - April 2025",
+    "Summer Internship Program Registration Open",
+  ];
+  
+  export const quickLinks = [
+    "Academic Calendar",
+    "Student Portal",
+    "Research Publications",
+    "Faculty Directory",
+  ];
+  
+  export const contactInfo = {
+    address: "Department of CSD & CSIT",
+    email: "csd.csit@srkrec.edu.in",
+    phone: "+1 234 567 8900",
+  };
+  
+  export const title = "Welcome to CSD & CSIT Department";
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,20 @@
 import { Box, Container, Grid, Typography, Paper } from '@mui/material';
+import { title, departmentOverview, announcements, quickLinks, contactInfo } from './api/home';
 
 const Home = () => {
   return (
     <Container maxWidth="lg">
       <Box sx={{ py: 4 }}>
         <Typography variant="h1" gutterBottom>
-          Welcome to CSD & CSIT Department
+          {title}
         </Typography>
-        
+
         {/* Department Overview */}
         <Paper sx={{ p: 3, mb: 4 }}>
           <Typography variant="h2" gutterBottom>
-            Department Overview
+            {departmentOverview.title}
           </Typography>
-          <Typography>
-            The Department of Computer Science & Design and Computer Science & Information Technology
-            is committed to excellence in teaching, research, and innovation.
-          </Typography>
+          <Typography>{departmentOverview.description}</Typography>
         </Paper>
 
         {/* Announcements */}
@@ -27,15 +25,11 @@ const Home = () => {
                 Announcements & News
               </Typography>
               <Box component="ul" sx={{ pl: 2 }}>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  New Research Lab Opening Ceremony - March 15, 2025
-                </Typography>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  International Conference on Emerging Technologies - April 2025
-                </Typography>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  Summer Internship Program Registration Open
-                </Typography>
+                {announcements.map((announcement, index) => (
+                  <Typography component="li" key={index} sx={{ mb: 1 }}>
+                    {announcement}
+                  </Typography>
+                ))}
               </Box>
             </Paper>
           </Grid>
@@ -47,18 +41,11 @@ const Home = () => {
                 Quick Links
               </Typography>
               <Box component="ul" sx={{ pl: 2 }}>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  Academic Calendar
-                </Typography>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  Student Portal
-                </Typography>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  Research Publications
-                </Typography>
-                <Typography component="li" sx={{ mb: 1 }}>
-                  Faculty Directory
-                </Typography>
+                {quickLinks.map((link, index) => (
+                  <Typography component="li" key={index} sx={{ mb: 1 }}>
+                    {link}
+                  </Typography>
+                ))}
               </Box>
             </Paper>
           </Grid>
@@ -70,10 +57,11 @@ const Home = () => {
             Contact Information
           </Typography>
           <Typography>
-            Department of CSD & CSIT<br />
-            University Campus<br />
-            Email: csd.csit@university.edu<br />
-            Phone: +1 234 567 8900
+            {contactInfo.address}
+            <br />
+            Email: {contactInfo.email}
+            <br />
+            Phone: {contactInfo.phone}
           </Typography>
         </Paper>
       </Box>
